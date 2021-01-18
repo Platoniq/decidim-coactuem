1e20816ebcc73c5b09a11b6dd727521f9b53202e
Ivan Vergés <ivan@platoniq.net>
2021-01-18 16:43:17 +0100

    setup sentry

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,12 @@ module Initial
 end
 
 unless ENV["DISABLE_SENTRY"]
-  Raven.configure do |config|
+  Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
+    config.breadcrumbs_logger = [:active_support_logger]
+
+    # To activate performance monitoring, set one of these options.
+    # We recommend adjusting the value in production:
+    config.traces_sample_rate = 0.5
   end
 end
