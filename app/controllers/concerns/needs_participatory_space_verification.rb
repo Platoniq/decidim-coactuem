395521a16fdac6539327c5676c6d80212a5e8742
Ivan Vergés <ivan@platoniq.net>
2021-11-10 15:07:41 +0100

    configure verifications in participatory processes (#23)

---
+++ app/controllers/concerns/needs_participatory_space_verification.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module NeedsParticipatorySpaceVerification
+  extend ActiveSupport::Concern
+
+  included do
+    def authorize_participatory_space
+      enforce_permission_to :read, :participatory_space, current_participatory_space: current_participatory_space
+      restricted_participatory_space?
+    end
+
+    private
+
+    def restricted_participatory_space?
+      restricted = Rails.application.secrets.restricted_participatory_spaces
+      return unless restricted && current_participatory_space
+
+      _name, spaces = restricted.find { |element| element[0] == current_participatory_space.manifest.name }
+      return unless spaces
+
+      space, methods = spaces.find { |item| item[0].to_s == current_participatory_space.slug }
+      return unless space && methods
+
+      redirect_to_first_authorization(methods)
+    end
+
+    def redirect_to_first_authorization(methods)
+      return if Decidim::Verifications::Authorizations.new(organization: current_organization, user: current_user, name: methods).any?
+
+      path = Decidim::Verifications::Adapter.from_element(methods.first).root_path(redirect_url: Decidim::ResourceLocatorPresenter.new(current_participatory_space)&.path)
+      redirect_to(path) || "/"
+    end
+  end
+end
